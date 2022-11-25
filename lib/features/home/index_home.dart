183d0dc24639ea7a2e940a388bbf4703897b6cc6
DRucci-88 <60127414+DRucci-88@users.noreply.github.com>
2022-11-25 17:32:59 +0700

    Home Overview Placeholder

--- lib/features/home/index_home.dart
+++ lib/features/home/index_home.dart
@@ -6,10 +6,346 @@ class IndexHome extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: const Center(
-        child: Text('Home Alelilooooo'),
+      // appBar: AppBar(),
+      body: Stack(children: [
+        Container(
+          height: 150,
+          decoration: const BoxDecoration(
+            color: Colors.blue,
+            borderRadius: BorderRadius.only(
+              bottomLeft: Radius.circular(20),
+              bottomRight: Radius.circular(20),
+            ),
+          ),
+        ),
+        _body(),
+      ]),
+    );
+  }
+
+  /// Body Safe Area
+  Widget _body() {
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              /// Custom App Bar
+              _customAppBar(),
+
+              /// Card Header
+              _cardHeader(),
+
+              /// Menu
+              _menu(),
+
+              /// List of Health Workers / Daftar Tenaga Kesehatan
+              _listOfHealthWorkers(),
+
+              /// Health News / Berita Kesehatan
+              _healthNews()
+            ],
+          ),
+        ),
       ),
+    );
+  }
+
+  /// Custom App bar
+  Widget _customAppBar() {
+    String temp1 = 'Hai Namamu';
+    return Row(
+      children: [
+        IconButton(
+          onPressed: () {},
+          icon: const Icon(Icons.person, color: Colors.white),
+        ),
+        Text(
+          temp1,
+          style: const TextStyle(color: Colors.white),
+        ),
+        const Spacer(),
+        IconButton(
+          onPressed: () {},
+          icon: const Icon(Icons.ring_volume),
+          color: Colors.white,
+        )
+      ],
+    );
+  }
+
+  /// Card Header
+  Widget _cardHeader() {
+    return Card(
+      elevation: 3,
+      child: Padding(
+        padding: const EdgeInsets.only(
+          left: 16.0,
+          right: 16.0,
+          top: 16.0,
+          bottom: 8.0,
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            Row(
+              children: [
+                const FlutterLogo(size: 46),
+                const SizedBox(width: 16.0),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: const [
+                      Text(
+                        'Ayo Lakukan Vaksinasi !',
+                        style: TextStyle(
+                            fontSize: 24, fontWeight: FontWeight.bold),
+                      ),
+                      Text(
+                          'Pastikan lengkapi data diri kamu sebelum melakukan pemesanan'),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text('Daftar Antrian'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  /// Menu
+  Widget _menu() {
+    /// Later will be replaced with model within this view model
+    List<Map<String, String>> datas = [
+      {
+        'label': 'Varietas Vaksin',
+        'image': '',
+      },
+      {
+        'label': 'Toko Kesehatan',
+        'image': '',
+      },
+      {
+        'label': 'Chat Rumah Sakit',
+        'image': '',
+      },
+      {
+        'label': 'Lihat\n Semua',
+        'image': '',
+      },
+    ];
+
+    return Padding(
+      padding: const EdgeInsets.all(16),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          for (var data in datas)
+            SizedBox(
+              width: 80,
+              height: 100,
+              child: Column(
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                      border: Border.all(width: 1),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: const FlutterLogo(size: 48),
+                  ),
+                  const SizedBox(height: 8),
+                  Text(
+                    data['label']!,
+                    textAlign: TextAlign.center,
+                    softWrap: true,
+                  ),
+                ],
+              ),
+            ),
+        ],
+      ),
+    );
+  }
+
+  /// List of Health Workers / Daftar Tenaga Kesehatan
+  Widget _listOfHealthWorkers() {
+    /// Later will be replaced with model within this view model
+    List<Map<String, String>> datas = [
+      {
+        'name': 'Dr.Abdul Syukur,S.Pd-KHOM',
+        'specialist': 'Spesialis kancker',
+        'desc': 'Prod Abdul adalah dokter yang mantapu jiwa GG gaming pokoknya'
+      },
+      {
+        'name': 'Prof.Hiru Hulukk,S.Pd-KHOM',
+        'specialist': 'Spesialis katarakk',
+        'desc': 'Dokter Huluk gemar menyuntik orang dengan suntikan cinta'
+      },
+    ];
+    return Column(
+      children: [
+        Row(
+          children: [
+            const Text(
+              'Daftar Tenaga Kesehatan',
+              style: TextStyle(fontSize: 18),
+            ),
+            const Spacer(),
+            TextButton(
+              onPressed: () {},
+              child: const Text('Lihat Semua'),
+            )
+          ],
+        ),
+        for (var data in datas)
+          Card(
+            elevation: 3,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: const FlutterLogo(size: 56),
+                  ),
+                  const SizedBox(width: 8.0),
+                  Expanded(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          data['name']!,
+                          style: const TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 18,
+                          ),
+                        ),
+                        const SizedBox(height: 8),
+                        Text(
+                          data['specialist']!,
+                          style: const TextStyle(
+                            color: Colors.blue,
+                            fontSize: 14,
+                          ),
+                        ),
+                        const SizedBox(height: 8),
+                        Text(
+                          '${data['desc']!.substring(0, 45 - 3)}...',
+                          style: const TextStyle(
+                            fontSize: 14,
+                            color: Colors.black54,
+                          ),
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            TextButton(
+                              onPressed: () {},
+                              child: const Text('Selengkapnya'),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+      ],
+    );
+  }
+
+  /// Health News / Berita Kesehatan
+  Widget _healthNews() {
+    /// Later will be replaced with model within this view model
+    List<Map<String, String>> datas = [
+      {
+        'title': 'Ini hal penting yang wajib kamu ketahui',
+        'author': 'Kobo Kanaeru',
+        'date': '12/09/2022 09:31',
+        'views': '23',
+      }
+    ];
+    return Column(
+      children: [
+        Row(
+          children: [
+            const Text(
+              'Berita Kesehatan',
+              style: TextStyle(fontSize: 18),
+            ),
+            const Spacer(),
+            TextButton(
+              onPressed: () {},
+              child: const Text('Lihat Semua'),
+            ),
+          ],
+        ),
+        for (var data in datas)
+          Card(
+            elevation: 3,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      // image: DecorationImage(image: )
+                    ),
+                    child: const FlutterLogo(
+                      size: 76,
+                      style: FlutterLogoStyle.stacked,
+                    ),
+                  ),
+                  const SizedBox(width: 8.0),
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          data['title']!,
+                          style: const TextStyle(fontSize: 18),
+                        ),
+                        const SizedBox(height: 8),
+                        Text(
+                          '${data['author']!} - ${data['date']!}',
+                          style: const TextStyle(color: Colors.black54),
+                        ),
+                        const SizedBox(height: 8),
+                        Row(
+                          children: [
+                            const Icon(
+                              Icons.remove_red_eye_outlined,
+                              color: Colors.blue,
+                            ),
+                            const SizedBox(width: 4),
+                            Text(
+                              '${data['views']!} views',
+                              style: const TextStyle(color: Colors.blue),
+                            )
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+      ],
     );
   }
 }
