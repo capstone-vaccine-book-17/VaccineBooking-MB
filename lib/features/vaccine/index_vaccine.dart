987a296bf447ae1c2ee0f7b26e6c212d1676adb3
RuslanIk06 <ruslan.andev@gmail.com>
2022-11-25 18:17:45 +0700

    Setup Project Feature Vaccine

--- lib/features/vaccine/index_vaccine.dart
+++ lib/features/vaccine/index_vaccine.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:w_vaccine/styles/custom_color.dart';
 
 class IndexVaccine extends StatelessWidget {
   const IndexVaccine({super.key});
@@ -6,7 +7,21 @@ class IndexVaccine extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        title: const Text(
+          "Booking Vaccine",
+          style: TextStyle(color: blackColor),
+        ),
+        leading: IconButton(
+          onPressed: () {},
+          icon: Image.asset(
+            "assets/vaccine/ic_back.png",
+            height: 15,
+            width: 15,
+          ),
+        ),
+      ),
       body: const Center(
         child: Text('Vaccine'),
       ),
---
+++ lib/styles/custom_color.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+const Color greyColor = Color(0xff888888);
+const Color blackColor = Color(0xff434446);
+const Color greenColor = Color(0xff1CBF57);
