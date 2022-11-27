0c01d240c5864d005f9baa08c7e17f40ce5522e1
unknown <benediktus@students.amikom.ac.id>
2022-11-27 12:11:39 +0700

    add data in map

--- lib/features/profile/anggota_keluarga.dart
+++ lib/features/profile/anggota_keluarga.dart
@@ -5,242 +5,158 @@ class AnggotaKeluarga extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    List<Map<String, String>> datas = [
+      {
+        'nama': 'Afifah Cahyaningsih',
+        'nik': '1312432423423',
+        'hub': 'Pribadi',
+        'dom': 'Jakarta',
+      },
+      {
+        'nama': 'Afifah Cahyaningsih',
+        'nik': '1312432423423',
+        'hub': 'Pribadi',
+        'dom': 'Jakarta',
+      },
+      {
+        'nama': 'Afifah Cahyaningsih',
+        'nik': '1312432423423',
+        'hub': 'Pribadi',
+        'dom': 'Jakarta',
+      },
+      {
+        'nama': 'Afifah Cahyaningsih',
+        'nik': '1312432423423',
+        'hub': 'Pribadi',
+        'dom': 'Jakarta',
+      },
+    ];
     return Scaffold(
       backgroundColor: Colors.white,
       body: SafeArea(
-        child: Column(
-          children: [
-            Container(
-              decoration: BoxDecoration(
-                color: Colors.white,
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.grey.withOpacity(0.2),
-                    spreadRadius: 0,
-                    blurRadius: 6,
-                    offset: const Offset(4, 4),
-                  )
-                ],
-              ),
-              child: Padding(
-                padding:
-                    const EdgeInsets.symmetric(vertical: 13, horizontal: 10),
-                child: Row(
-                  children: [
-                    IconButton(
-                      onPressed: () {
-                        Navigator.pop(context);
-                      },
-                      icon: const Icon(
-                        Icons.arrow_back,
-                        size: 32,
-                      ),
-                    ),
-                    const SizedBox(
-                      width: 10,
-                    ),
-                    const Text(
-                      'Anggota Keluarga',
-                      style:
-                          TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                    ),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.2),
+                      spreadRadius: 0,
+                      blurRadius: 6,
+                      offset: const Offset(4, 4),
+                    )
                   ],
                 ),
-              ),
-            ),
-            const SizedBox(height: 20),
-            Center(
-              child: Column(
-                children: [
-                  Padding(
-                    padding:
-                        const EdgeInsets.symmetric(horizontal: 20, vertical: 7),
-                    child: Container(
-                      width: double.infinity,
-                      height: 100,
-                      decoration: BoxDecoration(
-                        borderRadius:
-                            const BorderRadius.all(Radius.circular(10)),
-                        color: Colors.white,
-                        boxShadow: [
-                          BoxShadow(
-                            color: Colors.grey.withOpacity(0.3),
-                            spreadRadius: 0,
-                            blurRadius: 6,
-                            offset: const Offset(0, 0),
-                          )
-                        ],
-                      ),
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(
-                            vertical: 10, horizontal: 12),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: const [
-                                Text(
-                                  'Afifah Cahyaningsih',
-                                  style: TextStyle(fontWeight: FontWeight.bold),
-                                ),
-                                Text('Pribadi',
-                                    style:
-                                        TextStyle(fontWeight: FontWeight.bold)),
-                              ],
-                            ),
-                            const SizedBox(
-                              height: 12,
-                            ),
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: const [
-                                Text(
-                                  'NIK: 1231432424',
-                                  style: TextStyle(color: Colors.grey),
-                                ),
-                                Text('Jawa Barat',
-                                    style: TextStyle(color: Colors.grey)),
-                              ],
-                            ),
-                          ],
+                child: Padding(
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 13, horizontal: 10),
+                  child: Row(
+                    children: [
+                      IconButton(
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                        icon: const Icon(
+                          Icons.arrow_back,
+                          size: 32,
                         ),
                       ),
-                    ),
+                      const SizedBox(
+                        width: 10,
+                      ),
+                      const Text(
+                        'Anggota Keluarga',
+                        style: TextStyle(
+                            fontSize: 20, fontWeight: FontWeight.bold),
+                      ),
+                    ],
                   ),
-                  Padding(
-                    padding:
-                        const EdgeInsets.symmetric(horizontal: 20, vertical: 7),
-                    child: Container(
-                      width: double.infinity,
-                      height: 100,
-                      decoration: BoxDecoration(
-                        borderRadius:
-                            const BorderRadius.all(Radius.circular(10)),
-                        color: Colors.white,
-                        boxShadow: [
-                          BoxShadow(
-                            color: Colors.grey.withOpacity(0.3),
-                            spreadRadius: 0,
-                            blurRadius: 6,
-                            offset: const Offset(0, 0),
-                          )
-                        ],
-                      ),
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(
-                            vertical: 10, horizontal: 12),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: const [
-                                Text('Afifah Cahyaningsih',
-                                    style:
-                                        TextStyle(fontWeight: FontWeight.bold)),
-                                Text('Pribadi',
-                                    style:
-                                        TextStyle(fontWeight: FontWeight.bold)),
-                              ],
-                            ),
-                            const SizedBox(
-                              height: 12,
-                            ),
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: const [
-                                Text('NIK: 1231432424',
-                                    style: TextStyle(color: Colors.grey)),
-                                Text('Jawa Barat',
-                                    style: TextStyle(color: Colors.grey)),
-                              ],
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                  ),
-                  Padding(
-                    padding:
-                        const EdgeInsets.symmetric(horizontal: 20, vertical: 7),
-                    child: Container(
-                      width: double.infinity,
-                      height: 100,
-                      decoration: BoxDecoration(
-                        borderRadius:
-                            const BorderRadius.all(Radius.circular(10)),
-                        color: Colors.white,
-                        boxShadow: [
-                          BoxShadow(
-                            color: Colors.grey.withOpacity(0.3),
-                            spreadRadius: 0,
-                            blurRadius: 6,
-                            offset: const Offset(0, 0),
-                          )
-                        ],
-                      ),
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(
-                            vertical: 10, horizontal: 12),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: const [
-                                Text('Afifah Cahyaningsih',
-                                    style:
-                                        TextStyle(fontWeight: FontWeight.bold)),
-                                Text('Pribadi',
-                                    style:
-                                        TextStyle(fontWeight: FontWeight.bold)),
-                              ],
-                            ),
-                            const SizedBox(
-                              height: 12,
-                            ),
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: const [
-                                Text('NIK: 1231432424',
-                                    style: TextStyle(color: Colors.grey)),
-                                Text('Jawa Barat',
-                                    style: TextStyle(color: Colors.grey)),
-                              ],
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            const SizedBox(
-              height: 30,
-            ),
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                backgroundColor: const Color(0xff3366FF),
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 145, vertical: 15),
-                textStyle:
-                    const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(12), // <-- Radius
                 ),
               ),
-              onPressed: () {},
-              child: const Text(
-                "Tambah",
-                style: TextStyle(
-                  color: Color(0xffffffff),
-                  fontSize: 16,
+              const SizedBox(height: 20),
+              for (var data in datas)
+                Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 20, vertical: 7),
+                  child: Container(
+                    width: double.infinity,
+                    height: 100,
+                    decoration: BoxDecoration(
+                      borderRadius: const BorderRadius.all(Radius.circular(10)),
+                      color: Colors.white,
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey.withOpacity(0.3),
+                          spreadRadius: 0,
+                          blurRadius: 6,
+                          offset: const Offset(0, 0),
+                        )
+                      ],
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(
+                          vertical: 10, horizontal: 12),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(
+                                data['nama']!,
+                                style: TextStyle(fontWeight: FontWeight.bold),
+                              ),
+                              Text(data['hub']!,
+                                  style:
+                                      TextStyle(fontWeight: FontWeight.bold)),
+                            ],
+                          ),
+                          const SizedBox(
+                            height: 12,
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(
+                                'NIK: ' + data['nik']!,
+                                style: TextStyle(color: Colors.grey),
+                              ),
+                              Text(data['dom']!,
+                                  style: TextStyle(color: Colors.grey)),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              const SizedBox(
+                height: 30,
+              ),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: const Color(0xff3366FF),
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 145, vertical: 15),
+                  textStyle: const TextStyle(
+                      fontSize: 16, fontWeight: FontWeight.bold),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(12), // <-- Radius
+                  ),
+                ),
+                onPressed: () {},
+                child: const Text(
+                  "Tambah",
+                  style: TextStyle(
+                    color: Color(0xffffffff),
+                    fontSize: 16,
+                  ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
