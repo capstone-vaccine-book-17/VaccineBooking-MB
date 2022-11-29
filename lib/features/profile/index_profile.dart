d88501272dd75d864d1e6128616071eec2c12894
DRucci-88 <60127414+DRucci-88@users.noreply.github.com>
2022-11-29 17:31:39 +0700

    minor

--- lib/features/profile/index_profile.dart
+++ lib/features/profile/index_profile.dart
@@ -38,109 +38,107 @@ class _IndexProfileState extends State<IndexProfile> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.white,
-        body: SafeArea(
+      backgroundColor: Colors.white,
+      body: SafeArea(
           child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const Padding(
-                padding: EdgeInsets.symmetric(vertical: 30, horizontal: 25),
-                child: Text(
-                  'Profile',
-                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Padding(
+            padding: EdgeInsets.symmetric(vertical: 30, horizontal: 25),
+            child: Text(
+              'Profile',
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+          ),
+          Container(
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: const BorderRadius.only(
+                  bottomLeft: Radius.circular(14),
+                  bottomRight: Radius.circular(14)),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey.withOpacity(0.2),
+                  spreadRadius: 0,
+                  blurRadius: 6,
+                  offset: const Offset(4, 4),
                 ),
-              ),
-              Container(
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: const BorderRadius.only(
-                      bottomLeft: Radius.circular(14),
-                      bottomRight: Radius.circular(14)),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.grey.withOpacity(0.2),
-                      spreadRadius: 0,
-                      blurRadius: 6,
-                      offset: const Offset(4, 4),
-                    ),
-                  ],
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.only(bottom: 24),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: [
-                      const CircleAvatar(
-                        radius: 55,
-                        backgroundColor: Colors.grey,
-                      ),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: const [
-                          Text(
-                            'Halo, Afifah Cahyaningsih',
-                            style: TextStyle(
-                                fontSize: 20, fontWeight: FontWeight.bold),
-                          ),
-                          SizedBox(height: 18),
-                          Text(
-                            'NIK: 153424982422',
-                            style: TextStyle(
-                                fontSize: 18,
-                                fontWeight: FontWeight.w500,
-                                color: Colors.grey),
-                          ),
-                        ],
-                      )
-                    ],
+              ],
+            ),
+            child: Padding(
+              padding: const EdgeInsets.only(bottom: 24),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  const CircleAvatar(
+                    radius: 55,
+                    backgroundColor: Colors.grey,
                   ),
-                ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: const [
+                      Text(
+                        'Halo, Afifah Cahyaningsih',
+                        style: TextStyle(
+                            fontSize: 20, fontWeight: FontWeight.bold),
+                      ),
+                      SizedBox(height: 18),
+                      Text(
+                        'NIK: 153424982422',
+                        style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.w500,
+                            color: Colors.grey),
+                      ),
+                    ],
+                  )
+                ],
               ),
-              const SizedBox(height: 35),
-              ListMenu(
-                icon: Icons.person,
-                name: 'Data Diri',
-                func: (() {
-                  print('data diri ter click');
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => const PersonalDataPage()));
-                }),
-              ),
-              ListMenu(
-                icon: Icons.people_sharp,
-                name: 'Anggota Keluarga',
-                func: (() {
-                  print('Anggota Keluarga ter click');
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => const AnggotaKeluarga()));
-                }),
-              ),
-              ListMenu(
-                icon: Icons.lock,
-                name: 'Ubah Password',
-                func: (() {
-                  print('Ubah Password ter click');
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => const UbahPassword()));
-                }),
-              ),
-              ListMenu(
-                icon: Icons.exit_to_app_sharp,
-                name: 'Keluar Akun',
-                func: (() {
-                  print('Keluar Akun ter click');
-                }),
-              ),
-            ],
-          )
+            ),
+          ),
+          const SizedBox(height: 35),
+          ListMenu(
+            icon: Icons.person,
+            name: 'Data Diri',
+            func: (() {
+              print('data diri ter click');
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const PersonalDataPage()));
+            }),
+          ),
+          ListMenu(
+            icon: Icons.people_sharp,
+            name: 'Anggota Keluarga',
+            func: (() {
+              print('Anggota Keluarga ter click');
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const FamilyMembersPage()));
+            }),
+          ),
+          ListMenu(
+            icon: Icons.lock,
+            name: 'Ubah Password',
+            func: (() {
+              print('Ubah Password ter click');
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const ChangePasswordPage()));
+            }),
+          ),
+          ListMenu(
+            icon: Icons.exit_to_app_sharp,
+            name: 'Keluar Akun',
+            func: (() {
+              print('Keluar Akun ter click');
+            }),
+          ),
         ],
-      ),
+      )),
     );
   }
 }
