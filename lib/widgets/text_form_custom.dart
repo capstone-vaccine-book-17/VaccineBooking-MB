205be1edec115c445e120dce584617fd6eaf666a
DRucci-88 <60127414+DRucci-88@users.noreply.github.com>
2022-12-02 04:11:24 +0700

    update public widget for form

--- lib/widgets/text_form_custom.dart
+++ lib/widgets/text_form_custom.dart
@@ -10,6 +10,8 @@ class TextFormCustom extends StatelessWidget {
     this.textInputAction = TextInputAction.next,
     this.keyboardType = TextInputType.text,
     this.onFieldSubmitted,
+    this.valueListenablePass,
+    this.onTap,
   });
   final TextEditingController controller;
   final String hintText;
@@ -18,14 +20,33 @@ class TextFormCustom extends StatelessWidget {
   final TextInputAction textInputAction;
   final TextInputType keyboardType;
   final void Function(String value)? onFieldSubmitted;
+  final ValueNotifier<bool>? valueListenablePass;
+  final VoidCallback? onTap;
 
   @override
   Widget build(BuildContext context) {
+    Widget? suffixIcon() {
+      if (valueListenablePass == null) {
+        return null;
+      }
+      return IconButton(
+        onPressed: () {
+          valueListenablePass!.value = !valueListenablePass!.value;
+        },
+        icon: Icon(
+          valueListenablePass!.value ? Icons.visibility_off : Icons.visibility,
+        ),
+      );
+    }
+
     return TextFormField(
       focusNode: focusNode,
       controller: controller,
       textInputAction: textInputAction,
       keyboardType: keyboardType,
+      obscureText: valueListenablePass?.value ?? false,
+      onTap: onTap,
+      readOnly: onTap != null ? true : false,
       decoration: InputDecoration(
         hintText: hintText,
         border: const OutlineInputBorder(
@@ -33,6 +54,7 @@ class TextFormCustom extends StatelessWidget {
             Radius.circular(8),
           ),
         ),
+        suffixIcon: suffixIcon(),
       ),
       onFieldSubmitted: onFieldSubmitted,
       validator: validator,
